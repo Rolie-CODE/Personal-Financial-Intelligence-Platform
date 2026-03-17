ed55a20d666c6872ffab8dd221f52d12f2e48c21
Rolie-CODE <rytenkorang@st.ug.edy.gh>
2026-03-17 09:33:38 -0700

    Refactor user account management functions to improve password handling and account validation

--- main.py
+++ main.py
@@ -1,41 +1,63 @@
+import hashlib
+
 user_info = {
-    "account_name": {
-        "password":"account_password",
-        "email":"account_email"
-    }
+    
 }
 
+def hash_password(account_password):
+    hashed_password = hashlib.sha256(account_password.encode()).hexdigest()
+    return hashed_password
+
 def sign_up(new_account_name,new_password,new_email):
-    user_info[new_account_name] = {
-        "password": new_password,
-        "email": new_email
-    }
-    print("Account created successfully")
+    if new_account_name in user_info:
+        return"Account name already exists"
+    else:
+        hashed_password = hash_password(new_password)
+        user_info[new_account_name] = {
+            "password": hashed_password,
+            "email": new_email
+        }
+        return"Account created successfully"
 
 
 def sign_in(account_name,account_password):
     if account_name in user_info:
-        if account_password == user_info[account_name]["password"]:
-            print("Congratulations, you have logged in!")
+        hashed_password = hash_password(account_password)
+        if hashed_password == user_info[account_name]["password"]:
+            return "Congratulations, account created successfully"
+
+        else:
+           return "Incorrect password"
 
     else:
-        print("Account not found")
+       return "Account not found!"
 
 def forgot_password(account_name,new_account_password,account_email):
     if account_name in user_info:
         if account_email == user_info[account_name]["email"]:
-            if new_account_password == user_info[account_name]["password"]:
-                print("New password cannot be the same as old password")
+            hashed_new_password = hash_password(new_account_password)
+            if hashed_new_password == user_info[account_name]["password"]:
+                return("New password cannot be the same as old password")
             else:
-                user_info[account_name]["password"] = new_account_password
-                print("Password changed successfully")
+                user_info[account_name]["password"] = hashed_new_password
+                return("Password changed successfully")
         else:
-            print("Invalid email address")  
+            return("Invalid email address")  
     else:
-        print("Account does not exist")
+        return("Account does not exist")
 
-# sign_up("Rolandreads11","roro1234","rolandtenkoko@gmail.com")
-# forgot_password("Rolandreads11","roro1234","rolandtenkoko@gmail.com")
+def deactivate_account(account_name,account_password):
+    if account_name in user_info:
+        hashed_password = hash_password(account_password)
+        if hashed_password == user_info[account_name]["password"]:
+            del user_info[account_name]
+            return("Account deactivated")
+        else:
+            return("Incorrect password")
+    
+    else:
+        return("Account does not exist")
 
-
+# sign_up("Roland","12345","rolandtenkoko@gmail.com")
+# print(user_info)
         
