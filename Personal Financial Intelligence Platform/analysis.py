9e6892203ccec98bf13af6f81251a3b8b4d5d9ab
Rolie-CODE <rytenkorang@st.ug.edu.gh>
2026-03-19 23:20:41 -0700

    Implemented the get summary method

--- Personal Financial Intelligence Platform/analysis.py
+++ Personal Financial Intelligence Platform/analysis.py
@@ -1,17 +1,18 @@
 from storage import transaction_history
 
 def get_financial_summary(user):
+    income = 0
+    expenses = 0
+
     if user in transaction_history:
-        for amount in transaction_history.values():
-            if transaction_history["type"] == "income":
-                income += amount
-                return income
-            
-        for amount in transaction_history.values():
-            if transaction_history["type"] == "expenses":
-                expenses += amount
-                return expenses
-            
+
+        for transaction in transaction_history[user]:
+
+            if transaction["type"] == "income":
+                income += transaction["amount"]
+
+            elif transaction["type"] == "expenses":
+                expenses += transaction["amount"]
+
     amount_left = income - expenses
     return amount_left
-            
