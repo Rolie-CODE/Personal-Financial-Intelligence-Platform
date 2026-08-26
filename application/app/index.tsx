cfc375faa705f5a4fa58dc591bd0e3dd796c03f4
Rolie-CODE <rytenkorang@st.ug.edu.gh>
2026-08-25 22:44:09 -0700

    formatted the document with prettier

--- application/app/index.tsx
+++ application/app/index.tsx
@@ -1,60 +1,39 @@
-import {View, Text, TextInput, TouchableOpacity} from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
+export default function Sign_Up() {
+  return (
+    <View>
+      <View>
+        <Text>Lock</Text>
 
-export default function Sign_Up(){
-    return (
+        <Text>Sign In</Text>
+      </View>
+
+      <View>
+        <Text>Account Name</Text>
+
+        <TextInput placeholder="AccountName"></TextInput>
+      </View>
+
+      <View>
+        <Text>Password</Text>
+
+        <TextInput placeholder="......."></TextInput>
+      </View>
+
+      <TouchableOpacity>
+        <Text style={{ color: "white" }}>Sign In</Text>
+      </TouchableOpacity>
+
+      <View>
+        <Text>Forgot Password?</Text>
+
         <View>
-            <View>
-                <Text>
-                    Lock
-                </Text>
+          <Text>No account?</Text>
 
-                <Text>
-                    Sign In 
-                </Text>
-            </View>
-
-            <View>
-                <Text>
-                    Account Name
-                </Text>
-
-                <TextInput
-                placeholder='AccountName'>
-                </TextInput>
-            </View>
-
-            <View>
-                <Text>
-                    Password
-                </Text>
-
-                <TextInput
-                placeholder='.......'>
-                </TextInput>
-            </View>
-
-            <TouchableOpacity>
-                <Text style={{color: 'white'}}>
-                    Sign In
-                </Text>
-            </TouchableOpacity>
-
-            <View>
-                <Text>
-                    Forgot Password?
-                </Text>
-
-                <View>
-                    <Text>
-                        No account?
-                    </Text>
-
-                    <Text>
-                        Sign Up!
-                    </Text>
-                </View>
-            </View>
+          <Text>Sign Up!</Text>
         </View>
-    )
+      </View>
+    </View>
+  );
 }
