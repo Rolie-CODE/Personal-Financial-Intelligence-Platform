5fc9a5acd9b231f89f38704ad6affe84bdc7ff91
Rolie-CODE <rytenkorang@st.ug.edu.gh>
2026-08-25 22:41:51 -0700

    commit

--- application/app/index.tsx
+++ application/app/index.tsx
@@ -4,9 +4,7 @@ import {View, Text} from 'react-native'
 export default function Sign_Up(){
     return (
         <View>
-            <Text>
-                Hello I am <ROland></ROland>
-            </Text>
+            
         </View>
     )
 }
