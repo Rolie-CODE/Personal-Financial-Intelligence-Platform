9896342834649236240b6343538501f5de85a985
Rolie-CODE <rytenkorang@st.ug.edu.gh>
2026-08-25 22:42:36 -0700

    provided all the components for the home sign in screen

--- application/app/index.tsx
+++ application/app/index.tsx
@@ -1,10 +1,60 @@
-import {View, Text} from 'react-native'
+import {View, Text, TextInput, TouchableOpacity} from 'react-native'
 
 
 export default function Sign_Up(){
     return (
         <View>
-            
+            <View>
+                <Text>
+                    Lock
+                </Text>
+
+                <Text>
+                    Sign In 
+                </Text>
+            </View>
+
+            <View>
+                <Text>
+                    Account Name
+                </Text>
+
+                <TextInput
+                placeholder='AccountName'>
+                </TextInput>
+            </View>
+
+            <View>
+                <Text>
+                    Password
+                </Text>
+
+                <TextInput
+                placeholder='.......'>
+                </TextInput>
+            </View>
+
+            <TouchableOpacity>
+                <Text style={{color: 'white'}}>
+                    Sign In
+                </Text>
+            </TouchableOpacity>
+
+            <View>
+                <Text>
+                    Forgot Password?
+                </Text>
+
+                <View>
+                    <Text>
+                        No account?
+                    </Text>
+
+                    <Text>
+                        Sign Up!
+                    </Text>
+                </View>
+            </View>
         </View>
     )
 }
